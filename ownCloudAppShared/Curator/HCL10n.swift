b9a79deee89558d243486438445467b76be63e55
Mikhail Seriukov <mikhail.seryukov@noveogroup.com>
2026-02-04 11:10:13 +0200

    Integrate l10n script.

--- ownCloudAppShared/Curator/HCL10n.swift
+++ ownCloudAppShared/Curator/HCL10n.swift
@@ -1,191 +1,188 @@
+import ownCloudSDK
+
 public enum HCL10n {
 	public enum Common {
-		public static let ok = "OK"
-		public static let cancel = "Cancel"
+		public static var ok: String { OCLocalizedString("Common.ok" , nil) }
+		public static var cancel: String { OCLocalizedString("Common.cancel" , nil) }
 	}
 
 	public enum Logo {
-		public static let firstPart = "Personal Cloud "
-		public static let secondPart = "Files"
-	}
-
-	public enum Welcome {
-		public static let startSetupButtonTitle = "Start setup"
-		public static let settingsButtonTitle = "Settings"
+		public static var firstPart: String { OCLocalizedString("Logo.firstPart" , nil) }
+		public static var secondPart: String { OCLocalizedString("Logo.secondPart" , nil) }
 	}
 
 	public enum Onboarding {
-		public static let doneButtonTitle = "Done"
-		public static let skipButtonTitle = "Skip"
+		public static var doneButtonTitle: String { OCLocalizedString("Onboarding.doneButtonTitle" , nil) }
+		public static var skipButtonTitle: String { OCLocalizedString("Onboarding.skipButtonTitle" , nil) }
 
 		public enum Step_ManageFiles {
-			public static let title = "Welcome to Personal Cloud Files"
-			public static let subtitle = "Securely upload, manage, and share your files with Personal Cloud Files, your private gateway to personal storage.\n\nWhether you're organizing files or sharing documents with family and friends, Personal Cloud Files keeps your data safe with simple access."
+			public static var title: String { OCLocalizedString("Onboarding.Step_ManageFiles.title" , nil) }
+			public static var subtitle: String { OCLocalizedString("Onboarding.Step_ManageFiles.subtitle" , nil) }
 		}
 		public enum Step_SyncFiles {
-			public static let title = "Sync your files to Personal Cloud"
-			public static let subtitle = "Use the desktop app to upload files directly from your computer to Personal Cloud Files.\n\nEnsure your files are synced and backed up to Personal Cloud for easy access across all your devices."
+			public static var title: String { OCLocalizedString("Onboarding.Step_SyncFiles.title" , nil) }
+			public static var subtitle: String { OCLocalizedString("Onboarding.Step_SyncFiles.subtitle" , nil) }
 		}
 		public enum Step_FileDeduplication {
-			public static let title = "File deduplication"
-			public static let subtitle = "Identify and manage duplicate files uploaded to your account.\n\nSee exactly how many copies exist and access them all from the Duplicates page."
+			public static var title: String { OCLocalizedString("Onboarding.Step_FileDeduplication.title" , nil) }
+			public static var subtitle: String { OCLocalizedString("Onboarding.Step_FileDeduplication.subtitle" , nil) }
 		}
 		public enum Step_Search {
-			public static let title = "Quickly find what you need"
-			public static let subtitle = "Search files on Personal Cloud by name, date, type, size, and folder."
+			public static var title: String { OCLocalizedString("Onboarding.Step_Search.title" , nil) }
+			public static var subtitle: String { OCLocalizedString("Onboarding.Step_Search.subtitle" , nil) }
 		}
 		public enum Step_SecureSharing {
-			public static let title = "Secure sharing made simple"
-			public static let subtitle = "Share files and folders securely with family and friends. You can control access by setting expiration dates for shared links so that files remain private over time."
+			public static var title: String { OCLocalizedString("Onboarding.Step_SecureSharing.title" , nil) }
+			public static var subtitle: String { OCLocalizedString("Onboarding.Step_SecureSharing.subtitle" , nil) }
 		}
 	}
 
 	public enum Sidebar {
-		public static let storageSpace = "Storage space"
-		public static func used(_ a: String, of b: String) -> String { "\(a) of \(b) used" }
-		public static let unlimitedSpace = "Unlimited space"
-		public static let signOut = "Sign out"
+		public static var storageSpace: String { OCLocalizedString("Sidebar.storageSpace" , nil) }
+		public static func used(_ a: String, of b: String) -> String { String(format: OCLocalizedString("Sidebar.usedFormat" , nil), a, b) }
+		public static var unlimitedSpace: String { OCLocalizedString("Sidebar.unlimitedSpace" , nil) }
+		public static var signOut: String { OCLocalizedString("Sidebar.signOut" , nil) }
 	}
 
 	public enum TabBar {
-		public static let files = "Files"
-		public static let search = "Search"
-		public static let status = "Status"
-		public static let offline = "Offline"
+		public static var files: String { OCLocalizedString("TabBar.files", nil) }
+		public static var search: String { OCLocalizedString("TabBar.search", nil) }
+		public static var status: String { OCLocalizedString("TabBar.status", nil) }
+		public static var offline: String { OCLocalizedString("TabBar.offline", nil) }
 	}
 
 	public enum Search {
 		public enum Empty {
-			public static let title = "Search for your files"
+			public static var title: String { OCLocalizedString("Search.Empty.title", nil) }
 		}
 	}
 
 	public enum Offline {
 		public enum Empty {
-			public static let title = "No files available offline"
-			public static let subtitle = "Files and folders you mark as available offline will show up here."
+			public static var title: String { OCLocalizedString("Offline.Empty.title", nil) }
+			public static var subtitle: String { OCLocalizedString("Offline.Empty.subtitle", nil) }
 		}
 	}
 
 	public enum ImageCropAction {
-		public static let name = "Crop/rotate image"
+		public static var name: String { OCLocalizedString("ImageCropAction.name", nil) }
 
 		public enum SaveAlert {
-			public static let title = "Save File"
-			public static let overwrite = "Overwrite original"
-			public static let saveACopy = "Save as copy"
-			public static let discard = "Discard changes"
+			public static var title: String { OCLocalizedString("ImageCropAction.SaveAlert.title", nil) }
+			public static var overwrite: String { OCLocalizedString("ImageCropAction.SaveAlert.overwrite", nil) }
+			public static var saveACopy: String { OCLocalizedString("ImageCropAction.SaveAlert.saveACopy", nil) }
+			public static var discard: String { OCLocalizedString("ImageCropAction.SaveAlert.discard", nil) }
 		}
 	}
 
 	public enum Auth {
 		public enum Login {
-			public static let loadingViewTitle = "Logging in to your account"
-			public static let incorrectEmailPassword = "Incorrect email or password"
-			public static let invalidEmail = "Please enter a valid email address"
-			public static let connectionError = "Connection error. Unable to reach the server."
-			public static let notAllowedEmailError = "Not allowed. Contact the device owner"
+			public static var loadingViewTitle: String { OCLocalizedString("Auth.Login.loadingViewTitle", nil) }
+			public static var incorrectEmailPassword: String { OCLocalizedString("Auth.Login.incorrectEmailPassword", nil) }
+			public static var invalidEmail: String { OCLocalizedString("Auth.Login.invalidEmail", nil) }
+			public static var connectionError: String { OCLocalizedString("Auth.Login.connectionError", nil) }
+			public static var notAllowedEmailError: String { OCLocalizedString("Auth.Login.notAllowedEmailError", nil) }
 
 			public enum EmailField {
-				public static let title = "Email"
-				public static let placeholder = "Email"
+				public static var title: String { OCLocalizedString("Auth.Login.EmailField.title", nil) }
+				public static var placeholder: String { OCLocalizedString("Auth.Login.EmailField.placeholder", nil) }
 			}
 
 			public enum PasswordField {
-				public static let title = "Password"
-				public static let placeholder = "Password"
+				public static var title: String { OCLocalizedString("Auth.Login.PasswordField.title", nil) }
+				public static var placeholder: String { OCLocalizedString("Auth.Login.PasswordField.placeholder", nil) }
 			}
 
 			public enum DeviceDropdownField {
-				public static let title = "Connecting to"
-				public static let placeholder = "No device detected"
+				public static var title: String { OCLocalizedString("Auth.Login.DeviceDropdownField.title", nil) }
+				public static var placeholder: String { OCLocalizedString("Auth.Login.DeviceDropdownField.placeholder", nil) }
 			}
 
-			public static let loginButtonTitle = "Login"
-			public static let nextButtonTitle = "Next"
-			public static let resetPasswordButtonTitle = "Reset Password"
-			public static let detectingDevices = "Detecting devices..."
-			public static let noDeviceDetected = "No device detected"
+			public static var loginButtonTitle: String { OCLocalizedString("Auth.Login.loginButtonTitle", nil) }
+			public static var nextButtonTitle: String { OCLocalizedString("Auth.Login.nextButtonTitle", nil) }
+			public static var resetPasswordButtonTitle: String { OCLocalizedString("Auth.Login.resetPasswordButtonTitle", nil) }
+			public static var detectingDevices: String { OCLocalizedString("Auth.Login.detectingDevices", nil) }
+			public static var noDeviceDetected: String { OCLocalizedString("Auth.Login.noDeviceDetected", nil) }
 		}
 
 		public enum UnableToConnect {
-			public static let navbarTitle = "Unable to connect"
+			public static var navbarTitle: String { OCLocalizedString("Auth.UnableToConnect.navbarTitle", nil) }
 
-			public static let headerText = "We are unable to detect the Personal Cloud device. Please ensure the following:"
-			public static let point1 = "Verify that the device is properly connected to the network and that the internet connection is stable"
-			public static let point2 = "Make sure the device is powered on and functioning correctly"
-			public static let point3 = "Check firewall settings to confirm they are not blocking the connection"
-			public static let point4 = "If using a wireless connection, confirm the device is within range of the router"
-			public static let point5 = "Your device and mobile phone must be on the same network"
-			public static let footerText = "If you continue to experience issues, please contact our Support team for further assistance."
-			public static let footerLinkText = "Support team"
-			public static let retryButtonTitle = "Retry"
+			public static var headerText: String { OCLocalizedString("Auth.UnableToConnect.headerText", nil) }
+			public static var point1: String { OCLocalizedString("Auth.UnableToConnect.point1", nil) }
+			public static var point2: String { OCLocalizedString("Auth.UnableToConnect.point2", nil) }
+			public static var point3: String { OCLocalizedString("Auth.UnableToConnect.point3", nil) }
+			public static var point4: String { OCLocalizedString("Auth.UnableToConnect.point4", nil) }
+			public static var point5: String { OCLocalizedString("Auth.UnableToConnect.point5", nil) }
+			public static var footerText: String { OCLocalizedString("Auth.UnableToConnect.footerText", nil) }
+			public static var footerLinkText: String { OCLocalizedString("Auth.UnableToConnect.footerLinkText", nil) }
+			public static var retryButtonTitle: String { OCLocalizedString("Auth.UnableToConnect.retryButtonTitle", nil) }
 		}
 
 		public enum UnableToDetect {
-			public static let navbarTitle = "Unable to detect"
+			public static var navbarTitle: String { OCLocalizedString("Auth.UnableToDetect.navbarTitle", nil) }
 
-			public static let headerText = "We are unable to detect your device. Please ensure the following:"
-			public static let point1 = "Verify that Personal Cloud is properly connected to your home network"
-			public static let point2 = "Confirm that your internet has access to the web"
-			public static let point3 = "Make sure Personal Cloud is powered on"
-			public static let point4 = "Confirm that there are no firewall settings that block access to the internet"
-			public static let point5 = "If Personal Cloud is attempting to connect via Wi-Fi, confirm that it is in range of your home router"
-			public static let point6 = "Personal Cloud and your mobile device must be on the same network during the initial setup"
-			public static let footerText = "If you continue to experience issues, please contact our Support team for further assistance."
-			public static let footerLinkText = "Support team"
-			public static let retryButtonTitle = "Retry"
+			public static var headerText: String { OCLocalizedString("Auth.UnableToDetect.headerText", nil) }
+			public static var point1: String { OCLocalizedString("Auth.UnableToDetect.point1", nil) }
+			public static var point2: String { OCLocalizedString("Auth.UnableToDetect.point2", nil) }
+			public static var point3: String { OCLocalizedString("Auth.UnableToDetect.point3", nil) }
+			public static var point4: String { OCLocalizedString("Auth.UnableToDetect.point4", nil) }
+			public static var point5: String { OCLocalizedString("Auth.UnableToDetect.point5", nil) }
+			public static var point6: String { OCLocalizedString("Auth.UnableToDetect.point6", nil) }
+			public static var footerText: String { OCLocalizedString("Auth.UnableToDetect.footerText", nil) }
+			public static var footerLinkText: String { OCLocalizedString("Auth.UnableToDetect.footerLinkText", nil) }
+			public static var retryButtonTitle: String { OCLocalizedString("Auth.UnableToDetect.retryButtonTitle", nil) }
 		}
 
 		public enum CodeVerification {
-			public static let title = "Allow remote access"
-			public static let description = "We have sent a one-time code to authorize this device to access your Personal Cloud. Check your email for the code and enter it here. If you do not see it in your inbox, please check your spam folder."
+			public static var title: String { OCLocalizedString("Auth.CodeVerification.title", nil) }
+			public static var description: String { OCLocalizedString("Auth.CodeVerification.description", nil) }
 
-			public static let allowAccessButtonTitle = "Allow access"
-			public static let resendCodeButtonTitle = "Resend code"
-			public static let skipButtonTitle = "Skip"
+			public static var allowAccessButtonTitle: String { OCLocalizedString("Auth.CodeVerification.allowAccessButtonTitle", nil) }
+			public static var resendCodeButtonTitle: String { OCLocalizedString("Auth.CodeVerification.resendCodeButtonTitle", nil) }
+			public static var skipButtonTitle: String { OCLocalizedString("Auth.CodeVerification.skipButtonTitle", nil) }
 
-			public static let invalidCodeError = "Incorrect code."
-			public static let codeExpiredError = "Your code has expired."
-			public static let tooManyRequestsError = "Too many requests."
-			public static let connectionError = "Connection error. Unable to reach the server."
+			public static var invalidCodeError: String { OCLocalizedString("Auth.CodeVerification.invalidCodeError", nil) }
+			public static var codeExpiredError: String { OCLocalizedString("Auth.CodeVerification.codeExpiredError", nil) }
+			public static var tooManyRequestsError: String { OCLocalizedString("Auth.CodeVerification.tooManyRequestsError", nil) }
+			public static var connectionError: String { OCLocalizedString("Auth.CodeVerification.connectionError", nil) }
 		}
 
 		public enum Code500 {
-			public static let title = "Unable to connect"
-			public static let description = "Could not reach your Personal Cloud."
-			public static let cancelButtonTitle = "Cancel"
-			public static let retryButtonTitle = "Retry"
+			public static var title: String { OCLocalizedString("Auth.Code500.title", nil) }
+			public static var description: String { OCLocalizedString("Auth.Code500.description", nil) }
+			public static var cancelButtonTitle: String { OCLocalizedString("Auth.Code500.cancelButtonTitle", nil) }
+			public static var retryButtonTitle: String { OCLocalizedString("Auth.Code500.retryButtonTitle", nil) }
 		}
 		public enum CodeUnknownEmail {
-			public static let title = "Email not registered"
-			public static let description = "This email isn’t authorized to access this device. Please contact the owner."
-			public static let cancelButtonTitle = "OK"
+			public static var title: String { OCLocalizedString("Auth.CodeUnknownEmail.title", nil) }
+			public static var description: String { OCLocalizedString("Auth.CodeUnknownEmail.description", nil) }
+			public static var cancelButtonTitle: String { OCLocalizedString("Auth.CodeUnknownEmail.cancelButtonTitle", nil) }
 		}
 
 		public enum TooManyRequests {
-			public static let title = "Access to Personal Cloud"
-			public static let description = "There have been too many attempts for access within the last minute. Please wait at least two minutes before retrying."
-			public static let cancelButtonTitle = "OK"
+			public static var title: String { OCLocalizedString("Auth.TooManyRequests.title", nil) }
+			public static var description: String { OCLocalizedString("Auth.TooManyRequests.description", nil) }
+			public static var cancelButtonTitle: String { OCLocalizedString("Auth.TooManyRequests.cancelButtonTitle", nil) }
 		}
 
 		public enum DevOptions {
-			public static let title = "Developer options"
-			public static let deviceTextFieldTitle = "Static device"
-			public static let deviceTextFieldInvalidURLError = "Invalid URL"
-			public static let settingsSwitchLabel = "Display settings on login"
+			public static var title: String { OCLocalizedString("Auth.DevOptions.title", nil) }
+			public static var deviceTextFieldTitle: String { OCLocalizedString("Auth.DevOptions.deviceTextFieldTitle", nil) }
+			public static var deviceTextFieldInvalidURLError: String { OCLocalizedString("Auth.DevOptions.deviceTextFieldInvalidURLError", nil) }
+			public static var settingsSwitchLabel: String { OCLocalizedString("Auth.DevOptions.settingsSwitchLabel", nil) }
 		}
 	}
 
 	public enum Sharing {
-		public static let sharingNotPossible = "Sharing not possible"
-		public static let raNotAvilableDescription = "Sharing is not possible because remote access is not available."
-		public static let publicNotAvilableDescription = "Sharing is not possible because the public link is unavailable."
+		public static var sharingNotPossible: String { OCLocalizedString("Sharing.sharingNotPossible", nil) }
+		public static var raNotAvilableDescription: String { OCLocalizedString("Sharing.raNotAvilableDescription", nil) }
+		public static var publicNotAvilableDescription: String { OCLocalizedString("Sharing.publicNotAvilableDescription", nil) }
 	}
 
 	public enum TrustPrompt {
-		public static let title = "Untrusted server"
-		public static let messageFormat = "Do you want to trust the server at %@?"
-		public static let trust = "Trust"
+		public static var title: String { OCLocalizedString("TrustPrompt.title", nil) }
+		public static var messageFormat: String { OCLocalizedString("TrustPrompt.messageFormat", nil) }
+		public static var trust: String { OCLocalizedString("TrustPrompt.trust", nil) }
 	}
 }
